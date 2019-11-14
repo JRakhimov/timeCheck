d7ee501c49f3dac02ec4781ab04d67a191eb304e
RJ <rakhimov.5556@gmail.com>
2019-11-14 22:36:15 +0500

    fix: fetch accounts in every cron job

--- src/cron.ts
+++ src/cron.ts
@@ -3,57 +3,56 @@ import { CronJob } from "cron";
 
 import { Client } from "./whatsapp/Client";
 import { Logger, firebase } from "./utils";
+import { timezone } from "./config";
 
 const accountRegEx = /^[0-9]{0,}$/;
 
-interface CronOptions {
-  cronTime: string;
-  timezone: string;
-}
-
-interface MessageOptions {
-  accounts: Array<string>;
-  messageText: string;
-}
-
-export const cronJob = (whatsAppClient: Client, cronOptions: CronOptions, messageOptions: MessageOptions): CronJob => {
+export const cronJob = (whatsAppClient: Client, cronTime: string, messageText: string): CronJob => {
   const onTick = async (): Promise<void> => {
     const log = Logger("CronJob");
-    const moment = Moment().tz(cronOptions.timezone);
+    const moment = Moment().tz(timezone);
 
     if (whatsAppClient.pupPage) {
       log.info(`CronJob started: ${moment.format()}`);
 
       const isConnected = await whatsAppClient.isConnected();
 
-      for (const account of messageOptions.accounts) {
-        if (isConnected && accountRegEx.test(account)) {
-          const res = await whatsAppClient.sendMessage([account, "@c.us"].join(""), messageOptions.messageText);
-          const ref = `statistics/${moment.format("MMMM/DD/HH_mm")}/${account}`;
+      if (isConnected) {
+        const accounts: Array<string> = await firebase
+          .database()
+          .ref("accounts")
+          .once("value")
+          .then(x => Object.keys(x.val() || {}));
 
-          if (res === "success") {
-            const timestamp = new Date().getTime();
-            const promises = [
-              firebase
-                .database()
-                .ref(ref)
-                .set({
-                  sentDate: timestamp
-                }),
-              firebase
-                .database()
-                .ref(`accounts/${account}`)
-                .set({
-                  lastSentMessageDate: timestamp
-                })
-            ];
+        for (const account of accounts) {
+          if (accountRegEx.test(account)) {
+            const res = await whatsAppClient.sendMessage([account, "@c.us"].join(""), messageText);
+            const ref = `statistics/${moment.format("MMMM/DD/HH_mm")}/${account}`;
 
-            await Promise.all(promises);
+            if (res === "success") {
+              const timestamp = new Date().getTime();
+              const promises = [
+                firebase
+                  .database()
+                  .ref(ref)
+                  .set({
+                    sentDate: timestamp
+                  }),
+                firebase
+                  .database()
+                  .ref(`accounts/${account}`)
+                  .set({
+                    lastSentMessageDate: timestamp
+                  })
+              ];
+
+              await Promise.all(promises);
+            }
           }
         }
       }
     }
   };
 
-  return new CronJob(cronOptions.cronTime, onTick, undefined, false, cronOptions.timezone);
+  return new CronJob(cronTime, onTick, undefined, false, timezone);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,12 @@
 import WhatsAppClient from "./whatsapp";
-import { timezone } from "./config";
-import { firebase } from "./utils";
 import { cronJob } from "./cron";
 
 const Main = async (): Promise<void> => {
   const whatsAppClient = await WhatsAppClient(false);
 
-  const accounts: Array<string> = await firebase
-    .database()
-    .ref("accounts")
-    .once("value")
-    .then(x => Object.keys(x.val() || {}));
-
-  if (accounts.length) {
-    const cron = await cronJob(
-      whatsAppClient,
-      { cronTime: "*/30 * * * * *", timezone },
-      { messageText: "Cron test", accounts }
-    );
-    cron.stop();
-    // cron.start();
-  }
+  const cron = await cronJob(whatsAppClient, "*/30 * * * * *", "Cron test");
+  cron.stop();
+  // cron.start();
 };
 
 try {
